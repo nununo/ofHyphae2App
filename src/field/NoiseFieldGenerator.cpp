4a28df5b74f903500cbe9bbe7f04eacf7e4c2a54
nununo <mail@nunogodinho.com>
2021-06-26 02:57:25 +0100

    Tweak noise generator formula

--- src/field/NoiseFieldGenerator.cpp
+++ src/field/NoiseFieldGenerator.cpp
@@ -15,5 +15,6 @@ double NoiseFieldGenerator::getValue(const glm::vec2 normalizedPos) const {
   glm::vec2 pos = {normalizedPos.x + offset, normalizedPos.y + offset};
   auto noise1 = ofNoise(pos*1.5f); // pos*3
   noise1 = pow(noise1, 0.57f);
+  noise1 = ofMap(noise1, 0.2f, 1.0f, 0.0f, 1.0f, true);
   return noise1;
 }
