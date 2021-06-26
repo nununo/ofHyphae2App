3b36f7be8d5cf139703962cd04d968246b499993
nununo <mail@nunogodinho.com>
2021-06-26 02:58:01 +0100

    simplify main.app again

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,13 +6,15 @@ int main( ){
   ofSetupOpenGL(3072, 1920, OF_FULLSCREEN);      // <-------- setup the GL context
   ofRunApp(new ofApp());
 
-    //ofGLWindowSettings settings;
-  //settings.setGLVersion(3,2);
-  //settings.setSize(3072, 1920);
+  /*
+  ofGLWindowSettings settings;
+  settings.setGLVersion(3,2);
+  settings.setSize(3072, 1920);
     //  settings.windowMode = OF_WINDOW;
-  //settings.windowMode = OF_FULLSCREEN;
-  //auto mainWin = ofCreateWindow(settings);      // <-------- setup the GL context
-  //auto app = make_shared<ofApp>();
-  //ofRunApp( mainWin, app );
-  //ofRunMainLoop();
+  settings.windowMode = OF_FULLSCREEN;
+  auto mainWin = ofCreateWindow(settings);      // <-------- setup the GL context
+  auto app = make_shared<ofApp>();
+  ofRunApp( mainWin, app );
+  ofRunMainLoop();
+   */
 }
